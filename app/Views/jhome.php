6735f66fa6ced38acecdec683ba30abd0f31ffa4
Jef <130767801+Jef2326@users.noreply.github.com>
2023-10-06 06:54:57 +0800

    push

---
+++ app/Views/jhome.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<?= view('inc/head'); ?>
+<body>
+<?= view('inc/topbar'); ?>
+<?= view('inc/bar'); ?>
+<?= view('inc/bokal'); ?>
+<?= view('inc/modal'); ?>
+<?= view('inc/about'); ?>
+<?= view('inc/product'); ?>
+<?= view('inc/offer'); ?>
+<?= view('inc/pricing'); ?>
+<?= view('inc/team'); ?>
+<?= view('inc/test'); ?>
+<?= view('inc/footer'); ?>
+</body>
+</html>
